3fe6f59d0f04c96a66917a80c5c0d83348b18516
Manan Sharma <manansharma181105@gmail.com>
2026-06-08 09:39:37 +0530

    Add files via upload

---
+++ Week_2/Day_1/001_valid_anagram.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        count = {}
+        for ch in s:
+            count[ch] = count.get(ch, 0) + 1
+        for ch in t:
+            count[ch] = count.get(ch, 0) - 1
+        for value in count.values():
+            if value != 0:
+                return False
+        return True
